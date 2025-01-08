5e89686a3b77b5fe4f823b0512475161226243ca
Sonika <masteraimlinbits@gmail.com>
2025-01-08 21:37:21 +0530

    yml upd

--- tests/test_model.py
+++ tests/test_model.py
@@ -2,4 +2,4 @@ from model import train_model
 
 def test_train_model():
     acc = train_model()
-    assert acc > 0.5  # Simple test for accuracy threshold
+    assert acc > 0.8  # Simple test for accuracy threshold
