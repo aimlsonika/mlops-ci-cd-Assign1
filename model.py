a0ebc59879bb7452398c65d4e35d027949ea8466
Sonika <masteraimlinbits@gmail.com>
2025-01-09 09:57:37 +0530

    updated model.py

--- model.py
+++ model.py
@@ -7,13 +7,15 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
 
+
+
 def train_model():
     """
     Trains a logistic regression model on the Iris dataset.
     Returns the accuracy of the model on the test data.
     """
-    iris_data = load_iris()
-    x,y = iris_data.data, iris_data.target
+
+    x, y = load_iris()
     x_train, x_test, y_train, y_test = train_test_split(
         x, y, test_size=0.2, random_state=42)
     model = LogisticRegression(max_iter=200)
