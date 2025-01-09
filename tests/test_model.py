7d0b10c6d765f53a96d3b20bf33ea25008ec1d19
Sonika <masteraimlinbits@gmail.com>
2025-01-09 15:07:02 +0530

    renamed Repo

--- tests/test_model.py
+++ tests/test_model.py
@@ -5,7 +5,6 @@ import sys
 import os
 from ..model import train_model
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-
 def test_train_model():
     """
     Trains a logistic regression model on the Iris dataset.
