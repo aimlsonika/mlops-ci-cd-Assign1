dd798b6fd68fd7c601eff586bebedbef37ad3a77
Sonika <masteraimlinbits@gmail.com>
2025-01-09 10:35:37 +0530

    upd test_model.py

--- tests/test_model.py
+++ tests/test_model.py
@@ -1,5 +1,11 @@
 from model import train_model
-
+"""
+This script tests a logistic regression model on the Iris dataset.
+"""
 def test_train_model():
+    """
+    Trains a logistic regression model on the Iris dataset.
+    Asserts if the accuracy of the model on the test data is < 80%.
+    """
     acc = train_model()
     assert acc > 0.8  # Simple test for accuracy threshold
