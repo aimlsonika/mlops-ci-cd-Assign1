1eb6e641c3b54a08d50c3ec8496f8a908f100440
Sonika <masteraimlinbits@gmail.com>
2025-01-09 10:03:47 +0530

    Upd model.py

--- model.py
+++ model.py
@@ -1,20 +1,15 @@
 """
 This script trains a logistic regression model on the Iris dataset.
 """
-
 from sklearn.datasets import load_iris
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
-
-
-
 def train_model():
     """
     Trains a logistic regression model on the Iris dataset.
     Returns the accuracy of the model on the test data.
     """
-
     x, y = load_iris()
     x_train, x_test, y_train, y_test = train_test_split(
         x, y, test_size=0.2, random_state=42)
