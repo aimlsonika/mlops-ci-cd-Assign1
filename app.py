c308f03fc144b60c6bb9bc3826817dd7b78226ae
Sonika <masteraimlinbits@gmail.com>
2025-01-24 23:32:28 +0530

    upd req

--- app.py
+++ app.py
@@ -1,3 +1,6 @@
+"""
+This is flask application
+"""
 #Importing the Library
 import pandas as pd
 from flask import request
@@ -8,17 +11,17 @@ from sklearn.utils import validation
 # Initialize Flask application
 app = Flask(__name__)
 validation._IS_DEPRECATED_PICKLE = True  # This suppresses the warning
-model = joblib.load('mlops_ci_cd_assign1/modelfinal_best.joblib') 
-@app.route('/diabetesPrediction', methods=['POST'])      
-def diabetesPrediction():
-            data = request.json    
+model = joblib.load('mlops_ci_cd_assign1/modelfinal_best.joblib')
+@app.route('/diabetes_prediction', methods=['POST'])  
+def diabetes_prediction():
+            data = request.json
             df = pd.DataFrame(data["data"])
             #Drop if any features not required
             #df = df.drop(['Age','TemplateID'],axis=1)
             # Dataframe Creation
             df = df[['Pregnancies','Glucose','BloodPressure','SkinThickness',\
                 'Insulin','BMI','DiabetesPedigreeFunction','Age']]
-            df.Pregnancies 				= df.Pregnancies.astype('int64')               
+            df.Pregnancies 				= df.Pregnancies.astype('int64')              
             df.Glucose 					= df.Glucose.astype('int64')
             df.BloodPressure     		= df.BloodPressure.astype('int64')
             df.SkinThickness    		= df.SkinThickness.astype('int64')
@@ -26,11 +29,10 @@ def diabetesPrediction():
             df.BMI  					= df.BMI.astype('float64')
             df.DiabetesPedigreeFunction = df.DiabetesPedigreeFunction.astype('float64')
             df.Age   					= df.Age.astype('int64')
-     
             input = df.iloc[:,:]
             output = model.predict(input)
             final_predictions = pd.DataFrame(list(output),columns = \
                 ["Based on your test report, the diabetes result is:"])\
                     .to_dict(orient="records")
-            return jsonify(final_predictions) 
-app.run(debug=False,host = "127.0.0.1", port = 5000)   
+            return jsonify(final_predictions)
+app.run(debug=False,host = "127.0.0.1", port = 5000)  
