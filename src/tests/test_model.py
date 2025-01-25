d14cc0edab1de8ab9714813ce7a8b22dae198fc0
aimlsonika <masteraimlinbits@gmail.com>
2025-01-25 22:46:10 +0000

    new changes

--- src/tests/test_model.py
+++ src/tests/test_model.py
@@ -3,7 +3,7 @@ This script tests a logistic regression model with Optuna on the Diabetes datase
 """
 import sys
 import os
-import joblib 
+import joblib
 from src.tests.test_model import train_model_with_optuna, preprocess_data
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../src')))
 
